74092d753ee884ddcd91106090f9de8e424b5889
SwapRane12 <swaprr12@gmail.com>
2020-10-31 18:42:04 +0530

    initial bugfix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,10 @@
-import {Router} from 'express'
+import { Router } from 'express'
 import { Request, Response } from 'express'
 
-const router= Router()
+const router = Router()
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express Account' })
 })
-
 
 export default router
